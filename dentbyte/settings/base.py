4e489ffcff59ba0eec24a656e242c57dba002e8f
Sebastian Fuenamyor <sebasflyc@gmail.com>
2024-08-24 22:49:32 -0400

    Ejemplo de archivo .env

--- dentbyte/settings/base.py
+++ dentbyte/settings/base.py
@@ -27,10 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_kEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']
+DEBUG = os.environ['DEBUG'] == 'True'
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['*']  
  
 # Application definition
 
